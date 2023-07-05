737157ffefa823d47ac3035105f8c476fda04ab6
Muhammed Rajab <muhammedrajab101@gmail.com>
2023-07-06 00:47:13 +0530

    Cleaned the dll file for nwo, might have to add more stuff to the struct for better results

--- pkg/dll/dll.go
+++ pkg/dll/dll.go
@@ -1,5 +1,5 @@
 // Change this to dll
-package main
+package dll
 
 import "fmt"
 
@@ -7,9 +7,9 @@ import "fmt"
 // NODE
 // ////////////////////////////
 type Node struct {
-	val  string
-	prev *Node
-	next *Node
+	Val  string
+	Prev *Node
+	Next *Node
 }
 
 // Function to create a node
@@ -19,16 +19,16 @@ func CreateNode(val string, prev, next *Node) *Node {
 
 // Method to display a node
 func (node *Node) Display() {
-	fmt.Printf("value: %s\n", node.val)
+	fmt.Printf("value: %s\n", node.Val)
 }
 
 // Method to delete a node
 func (node *Node) Delete() {
-	if node.prev != nil {
-		node.prev.next = node.next
+	if node.Prev != nil {
+		node.Prev.Next = node.Next
 	}
-	if node.next != nil {
-		node.next.prev = node.prev
+	if node.Next != nil {
+		node.Next.Prev = node.Prev
 	}
 }
 
@@ -54,12 +54,11 @@ func (dll *DoublyLinkedList) Display() {
 	if temp := dll.Head(); temp != nil {
 		fmt.Printf("nil ⇄ ")
 		for temp != nil {
-			fmt.Printf("%s ⇄ ", temp.val)
-			temp = temp.next
+			fmt.Printf("%s ⇄ ", temp.Val)
+			temp = temp.Next
 		}
 		fmt.Printf("nil\n")
 	}
-
 }
 
 // Method to get length of the dll
@@ -69,7 +68,7 @@ func (dll *DoublyLinkedList) Length() uint32 {
 	temp := dll.Head()
 	for temp != nil {
 		len++
-		temp = temp.next
+		temp = temp.Next
 	}
 	return uint32(len)
 }
@@ -82,8 +81,8 @@ func (dll *DoublyLinkedList) Head() *Node {
 // Method to get Tail Node
 func (dll *DoublyLinkedList) Tail() *Node {
 	temp := dll.Head()
-	for temp.next != nil {
-		temp = temp.next
+	for temp.Next != nil {
+		temp = temp.Next
 	}
 	return temp
 }
@@ -91,8 +90,8 @@ func (dll *DoublyLinkedList) Tail() *Node {
 // Method to set Head Node
 func (dll *DoublyLinkedList) setHead(node *Node) *Node {
 	dll.head = node
-	node.next = nil
-	node.prev = nil
+	node.Next = nil
+	node.Prev = nil
 	return node
 }
 
@@ -105,9 +104,9 @@ func (dll *DoublyLinkedList) Append(node *Node) *Node {
 
 	// Case: 2 -> Head is not nil
 	tail := dll.Tail()
-	tail.next = node
-	node.prev = tail
-	node.next = nil
+	tail.Next = node
+	node.Prev = tail
+	node.Next = nil
 	return node
 }
 
@@ -119,10 +118,10 @@ func (dll *DoublyLinkedList) Prepend(node *Node) *Node {
 	}
 
 	// Case: 2 -> Head is not nil
-	node.next = dll.head
-	dll.head.prev = node
+	node.Next = dll.head
+	dll.head.Prev = node
 	dll.head = node
-	node.prev = nil
+	node.Prev = nil
 	return node
 }
 
@@ -133,28 +132,11 @@ func (dll *DoublyLinkedList) Pop() *Node {
 	}
 
 	tail := dll.Tail()
-	if tail.prev != nil {
-		tail.prev.next = nil
+	if tail.Prev != nil {
+		tail.Prev.Next = nil
 	} else {
 		dll.head = nil
 	}
-	tail.next = nil
+	tail.Next = nil
 	return tail
-}
-
-func main() {
-	dll := CreateDoublyLinkedListEmpty()
-	dll.Prepend(CreateNode("nigga", nil, nil))
-	dll.Prepend(CreateNode("bro", nil, nil))
-	dll.Append(CreateNode("2", nil, nil))
-	dll.Append(CreateNode("4", nil, nil))
-	dll.Append(CreateNode("3", nil, nil))
-	dll.Pop()
-	dll.Pop()
-	dll.Pop()
-	dll.Pop()
-	dll.Pop()
-	dll.Pop()
-	dll.Append(CreateNode("sdf", nil, nil))
-	dll.Display()
 }
