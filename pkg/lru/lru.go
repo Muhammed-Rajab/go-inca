63bd9438cf38c46cbdc017e241ee514c091acf07
Muhammed Rajab <muhammedrajab101@gmail.com>
2023-07-06 00:56:37 +0530

    Started working on lru

--- pkg/lru/lru.go
+++ pkg/lru/lru.go
@@ -1,4 +1,11 @@
 package lru
 
+import (
+	dll "github.com/Muhammed-Rajab/go-inca/pkg/dll"
+)
+
 type LRUCache struct {
+	capacity uint32
+	data     map[string]string
+	keys     *dll.DoublyLinkedList
 }
