c05079556dd5f0c85894eb1093f08789bb62d21d
Muhammed Rajab <muhammedrajab101@gmail.com>
2023-07-06 13:49:00 +0530

    Created a shit load of benchmark for inca, everything looks good expect the part where the capacity is already met

--- pkg/inca/inca_test.go
+++ pkg/inca/inca_test.go
@@ -1,16 +1,94 @@
 package inca
 
-// import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
-// // Benchmarking inca
-// func BenchmarkIncaSet(b *testing.B) {
+// Benchmarking inca
+func BenchmarkSetSameKey(b *testing.B) {
 
-// 	cache := CreateInca(&IncaConfig{
-// 		Capacity:       10000,
-// 		EvictionPolicy: TYPE_LRU,
-// 	})
+	cache := CreateInca(&IncaConfig{
+		Capacity:       10000,
+		EvictionPolicy: TYPE_LRU,
+	})
 
-// 	for i:= 0; i <= b.N; i++ {
-// 		cache.Memory.Set("i")
-// 	}
-// }
+	for i := 0; i <= b.N; i++ {
+		cache.Memory.Set("key", strconv.FormatInt(int64(i), 10))
+	}
+}
+
+func BenchmarkSetKeyTillCapacity(b *testing.B) {
+	// Not good results here on setting CAPACITY to 100000
+	const CAPACITY = 10000
+	cache := CreateInca(&IncaConfig{
+		Capacity:       CAPACITY,
+		EvictionPolicy: TYPE_LRU,
+	})
+
+	for i := 0; i <= CAPACITY; i++ {
+		kv := strconv.FormatInt(int64(i), 10)
+		cache.Memory.Set(kv, kv)
+	}
+}
+
+func BenchmarkSetKeyfor1MoreThanCapacity(b *testing.B) {
+	const CAPACITY = 1
+	cache := CreateInca(&IncaConfig{
+		Capacity:       CAPACITY,
+		EvictionPolicy: TYPE_LRU,
+	})
+
+	for i := 0; i <= CAPACITY+1; i++ {
+		kv := strconv.FormatInt(int64(i), 10)
+		cache.Memory.Set(kv, kv)
+	}
+}
+
+func BenchmarkSetKeyfor100MoreThanCapacity(b *testing.B) {
+	const CAPACITY = 1
+	cache := CreateInca(&IncaConfig{
+		Capacity:       CAPACITY,
+		EvictionPolicy: TYPE_LRU,
+	})
+
+	for i := 0; i <= CAPACITY+100; i++ {
+		kv := strconv.FormatInt(int64(i), 10)
+		cache.Memory.Set(kv, kv)
+	}
+}
+
+func BenchmarkSetKeyfor10xCapacity(b *testing.B) {
+	const CAPACITY = 10000
+	cache := CreateInca(&IncaConfig{
+		Capacity:       CAPACITY,
+		EvictionPolicy: TYPE_LRU,
+	})
+
+	for i := 0; i <= CAPACITY*10; i++ {
+		kv := strconv.FormatInt(int64(i), 10)
+		cache.Memory.Set(kv, kv)
+	}
+}
+func BenchmarkSetKey(b *testing.B) {
+	const CAPACITY = 10
+
+	cache := CreateInca(&IncaConfig{
+		Capacity:       CAPACITY,
+		EvictionPolicy: TYPE_LRU,
+	})
+
+	cache.Memory.Set("key", "value")
+}
+
+func BenchmarkSetGetKey(b *testing.B) {
+	const CAPACITY = 10
+
+	cache := CreateInca(&IncaConfig{
+		Capacity:       CAPACITY,
+		EvictionPolicy: TYPE_LRU,
+	})
+
+	cache.Memory.Set("key", "value")
+	cache.Memory.Get("key")
+}
